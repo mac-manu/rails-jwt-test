b98c099da553a1ec92a9d4d2439b058629c55a21
macmanu <manu@mac-manu.com>
2022-07-07 13:48:09 +0200

    auth controler plus routes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,14 @@
 class AuthenticationController < ApplicationController
+    skip_before_action :authenticate_request
+
+    #POST /auth/login 
+    def login
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            token = JsonWebToken.encode({user_id: @user.id})
+            render json: {token: token}, status: :ok
+        else
+            render json: {error: 'Invalid username or password'}, status: :unauthorized
+        end
+    end
 end
